f9c07775c0d45295d3c6d8d0bd5c2f85dae4c09d
ykrathore89 <110353331+ykrathore89@users.noreply.github.com>
2022-08-02 09:07:23 +0530

    Add files via upload

---
+++ Smart_Dustbin_Ultrasonic_Sensor_sketch_aug01a/Smart_Dustbin_Ultrasonic_Sensor_sketch_aug01a.ino
@@ -0,0 +1,65 @@
+#include <Servo.h>
+
+Servo servoMain; // Define our Servo
+
+int trigpin = 3;
+
+int echopin = 2;
+
+int distance;
+
+float duration;
+
+float cm;
+
+void setup()
+
+{
+
+servoMain.attach(9); // servo on digital pin 10
+
+pinMode(trigpin, OUTPUT);
+
+pinMode(echopin, INPUT);
+
+}
+
+void loop()
+
+{
+
+digitalWrite(trigpin, LOW);
+
+delay(2);
+
+digitalWrite(trigpin, HIGH);
+
+delayMicroseconds(10);
+
+digitalWrite(trigpin, LOW);
+
+duration = pulseIn(echopin, HIGH);
+
+cm = (duration/58.82);
+
+distance = cm;
+
+if(distance<30)
+
+{
+
+servoMain.write(180); // Turn Servo back to center position (90 degrees)
+
+delay(3000);
+
+}
+
+else{
+
+servoMain.write(0);
+
+delay(50);
+
+}
+
+}
